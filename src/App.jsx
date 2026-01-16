4e48a3e24315093ae00fe3cc0b166ac5b66bea94
Dhruv-Suyal <dhruvsuyal0@gmail.com>
2026-01-16 14:02:33 +0530

    ChatBot Project

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
-import { useState} from 'react'
+import { useState, useEffect} from 'react'
 import { ChatMessages } from './components/ChatMessages';
 import { ChatInput } from './components/ChatInput';
 import './App.css'
 
 
     function App(){ 
-        const [chatMessages, setChatMessages] = useState([]);
+        let localMessages = Array.isArray(JSON.parse(localStorage.getItem('message'))) ? JSON.parse(localStorage.getItem('message')) : [];
+        const [chatMessages, setChatMessages] = useState(localMessages);
+        
+        useEffect(()=>{
+            const stringMessages = JSON.stringify(chatMessages);
+            localStorage.setItem('message' , stringMessages);
+        },[chatMessages]);
+
         //const [chatMessages, setChatMessages] = array;       -----> It is destructuring of array
         //const chatMessages = array[0];
         //const setChatMessages = array[1];
